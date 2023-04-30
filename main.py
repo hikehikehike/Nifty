01d6ae46eaa4ec4b4c4504b45ea02ae6428626d6
Timur <timur.hike@gmail.com>
2023-04-30 23:53:19 +0300

    Delete API_KEY

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-import os
-
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import FileResponse
 from pathlib import Path
 
 from chatbot import load_embeddings, get_response
-from fastapi.security import APIKeyHeader
-from fastapi import Depends, HTTPException
 
 app = FastAPI()
 
 docsearch, chain = load_embeddings()
-API_KEY = os.getenv("API_KEY")
-
-api_key_header = APIKeyHeader(name="X-API-KEY")
-
-
-async def get_api_key(api_key_header: str = Depends(api_key_header)):
-    if api_key_header == API_KEY:
-        return api_key_header
-    else:
-        raise HTTPException(status_code=400, detail="Invalid API Key")
 
 
 @app.websocket("/ws")
@@ -37,7 +23,7 @@ async def websocket_endpoint(websocket: WebSocket):
         await websocket.send_text(response)
 
 
-@app.get("/", dependencies=[Depends(get_api_key)])
+@app.get("/")
 async def get():
     """
     GET endpoint that serves the index.html file.
